606557cd5cb3cfaf6a4c157c108f86c600fe8367
Afiz <afizshaik@in.ibm.com>
2022-07-09 13:03:33 +0530

    initial commit

---
+++ src/hello_emoji/example.py
@@ -0,0 +1,12 @@
+from random import randint
+
+EMOJIES = ['😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', 
+'🙃', '😊', '😇', '🥰', '😍', '😘', '😚', '😙', '🥲', '😋', '😛', '😜']
+
+def hello_emoji(name: str='') -> str:
+  return f'Hello {name}{EMOJIES[randint(0, 20)]}!'
+
+if __name__ == '__main__':
+  print(hello_emoji())
+  print(hello_emoji('Afiz'))
+
